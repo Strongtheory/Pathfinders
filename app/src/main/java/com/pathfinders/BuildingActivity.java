900e447aee19d22f2528ec23ef2a1ce13cffd78d
Connor Reeder <connor.reeder@ymail.com>
2016-10-16 16:17:37 -0400

    Populated ListView with static example building names

--- app/src/main/java/com/pathfinders/BuildingActivity.java
+++ app/src/main/java/com/pathfinders/BuildingActivity.java
@@ -2,11 +2,17 @@ package com.pathfinders;
 
 import android.app.ListActivity;
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Loader;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class BuildingActivity extends ListActivity
             implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -18,14 +24,15 @@ public class BuildingActivity extends ListActivity
     static final String[] BUILDINGS = new String[] {
             "Klaus",
             "CULC",
-            "Howey",
-            "Love Manufacturing"
+            "Howey"
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building);
+        BuildingAdapter mAdapter = new BuildingAdapter(this, BUILDINGS);
+        setListAdapter(mAdapter);
     }
 
     @Override
@@ -42,4 +49,21 @@ public class BuildingActivity extends ListActivity
     public void onLoaderReset(Loader<Cursor> loader) {
 
     }
+    private class BuildingAdapter extends ArrayAdapter<String> {
+        public BuildingAdapter(Context context, String[] buildings) {
+            super(context, 0, buildings);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            String buildingName = getItem(position);
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_building, parent, false);
+            }
+            TextView bldgName = (TextView) convertView.findViewById(R.id.bldgName);
+            bldgName.setText(buildingName);
+            return convertView;
+        }
+    }
 }
+
